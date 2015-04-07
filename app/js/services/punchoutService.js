e0493abb9dc988d9f606640f0a2ac0e456e1c58d
Kyle Olson <kolson@four51.com>
2015-04-07 15:12:12 -0500

    DEV-15582 Punchout - Procurement systems are unable to handle how we are encoding our base64 punchout order messages

--- app/js/services/punchoutService.js
+++ app/js/services/punchoutService.js
@@ -3,7 +3,7 @@ four51.app.factory('Punchout', ['$resource', '$451', '$http', '$cookieStore', fu
 	var _punchoutSession = $cookieStore.get('punchoutSession.' + $451.apiName);
 
 	var _getForm = function(success, error){
-		var url = $451.api('punchoutin/returnform/' + _punchoutSession.PunchoutTemplateName);
+        var url = $451.api('punchoutin/returnform/' + _punchoutSession.PunchoutTemplateName) + '?PunchoutBuyerCookie=' + _punchoutSession.PunchoutBuyerCookie;
 
 		$resource(url).get().$promise.then(function(form){
 				success(form);
